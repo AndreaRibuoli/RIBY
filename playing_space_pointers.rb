5a5c519eee795f2c76314eef1fc5e0c737d23cd0
Andrea Ribuoli <andrea@powerruby.com>
2021-02-28 17:27:55 +0100

    9 initial

--- playing_space_pointers.rb
+++ playing_space_pointers.rb
@@ -8,7 +8,7 @@ cmd  = "SNDPGMMSG MSG('#{ARGV[0]}') TOMSGQ(*SYSOPR)"
 size = cmd.length
 ILEpointer   = struct [ 'char b[16]' ]
 ILEarglist   = struct [ 'char c[64]' ]
-PASEpointer  = struct [ 'char d[8]' ]
+PASEpointer  = struct [ 'unsigned long p' ]
 preload    = Fiddle.dlopen(nil)
 ileloadx   = Fiddle::Function.new( preload['_ILELOADX'],
                            [Fiddle::TYPE_VOIDP, Fiddle::TYPE_INT],
@@ -29,7 +29,7 @@ if rc == 1 then
   ILEarguments[16, 16] = [ILEreturn.to_i.to_s(16).rjust(32,'0')].pack("H*")
   ILEarguments[32, 8]  = [size.to_s(16).rjust(16,'0')].pack("H*")
   ILEarguments[40, 0] = ['0'.rjust(16,'0')].pack("H*")
-  ILEarguments[48, 16] = [PASEpointer.to_i.to_s(16).rjust(32,'0')].pack("H*")
+  ILEarguments[48, 16] = [PASEreturn.to_i.to_s(16).rjust(32,'0')].pack("H*")
   rc = ilecallx.call(ILEfunction, ILEarguments, ['FFF8FFF50000'].pack("H*"), 16, 0)
   raise "ILE system failed with rc=#{rc}" if rc != 0
   puts "PASE pointer                 #{PASEreturn[0, 8].unpack("H*")}"
