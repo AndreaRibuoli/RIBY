44ab0346764c7b9937758679e5fa3e3b98973499
Andrea Ribuoli <andrea@powerruby.com>
2021-02-28 18:34:20 +0100

    9

--- playing_space_pointers_malloc.rb
+++ playing_space_pointers_malloc.rb
@@ -7,7 +7,6 @@ raise "Usage: playing_space_pointers.rb <size>" if ARGV.length != 1
 size = ARGV[0].to_i
 ILEpointer   = struct [ 'char b[16]' ]
 ILEarglist   = struct [ 'char c[36]' ]
-PASEpointer  = struct [ 'unsigned long p' ]
 preload    = Fiddle.dlopen(nil)
 ileloadx   = Fiddle::Function.new( preload['_ILELOADX'],
                            [Fiddle::TYPE_VOIDP, Fiddle::TYPE_INT],
@@ -26,13 +25,11 @@ rc = ilesymx.call(ILEfunction, ileloadx.call('QSYS/QC2UTIL1', 1), 'malloc')
 if rc == 1 then
   ILEreturn    = ILEpointer.malloc
   ILEarguments = ILEarglist.malloc
-  PASEreturn   = PASEpointer.malloc
   ILEarguments[0, 16]  = ['0'.rjust(32,'0')].pack("H*")
   ILEarguments[16, 16] = [ILEreturn.to_i.to_s(16).rjust(32,'0')].pack("H*")
   ILEarguments[32, 4]  = [size.to_s(16).rjust(8,'0')].pack("H*")
-  rc = ilecallx.call(ILEfunction, ILEarguments, ['FFFAFFF50000'].pack("H*"), 16, 0)
+  rc = ilecallx.call(ILEfunction, ILEarguments, ['FFFA0000'].pack("H*"), 16, 0)
   raise "ILE system failed with rc=#{rc}" if rc != 0
-  puts "PASE pointer                 #{PASEreturn[0, 8].unpack("H*")}"
   puts "ILE SPP      #{ILEreturn[0, 16].unpack("H*")}"
   puts "PASE pointer from _CVTSPP    [\"#{cvtspp.call(ILEreturn).to_s(16).rjust(16,'0')}\"]"
 end
