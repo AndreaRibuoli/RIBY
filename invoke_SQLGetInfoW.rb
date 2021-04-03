85cfe9108355a81e6c24b70fcb8f6eec77ef19a9
Andrea Ribuoli <andrea@powerruby.com>
2021-04-03 21:42:45 +0200

    21

--- invoke_SQLGetInfoW.rb
+++ invoke_SQLGetInfoW.rb
@@ -95,7 +95,7 @@ ILEarguments[  64,  2] = ['0100'].pack("H*")             # 256
 ILEarguments[  66, 14] = ['0'.rjust(28,'0')].pack("H*")  # padding
 ILEarguments[  80, 16] = [size.to_i.to_s(16).rjust(32,'0')].pack("H*")
 ILEarguments[  96, 48] = ['0'.rjust(96,'0')].pack("H*")
-rc = ilecallx.call(pSQLConnectW, ILEarguments, ['FFFBFFFDFFF5FFFDFFF50000'].pack("H*"), -5, 0)
+rc = ilecallx.call(pSQLGetInfoW, ILEarguments, ['FFFBFFFDFFF5FFFDFFF50000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
 puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
 puts ILEarguments[   0, 16].unpack("H*")
