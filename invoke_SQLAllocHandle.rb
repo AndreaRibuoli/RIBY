f87084af0c317d4e963e3a6feec4a9eab66af0e1
Andrea Ribuoli <andrea@powerruby.com>
2021-04-03 12:28:20 +0200

    20

--- invoke_SQLAllocHandle.rb
+++ invoke_SQLAllocHandle.rb
@@ -19,18 +19,17 @@ ilecallx   = Fiddle::Function.new( preload['_ILECALLX'],
                            Fiddle::TYPE_INT )
 ILEfunction  = ILEpointer.malloc
 rc = ilesymx.call(ILEfunction, ileloadx.call('QSYS/QSQCLI', 1), 'SQLAllocHandle')
-if rc == 1 then
-  handle = SQLhandle.malloc
-  ILEarguments = ILEarglist.malloc
-  ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
-  ILEarguments[ 32,  2] = ['0001'].pack("H*")             # htype
-  ILEarguments[ 34,  2] = ['0000'].pack("H*")             # padding
-  ILEarguments[ 36,  4] = ['00000000'].pack("H*")         # ihandle
-  ILEarguments[ 40,  8] = ['0000000000000000'].pack("H*") # padding
-  ILEarguments[ 48, 16] = [handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
-  rc = ilecallx.call(ILEfunction, ILEarguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
-  raise "ILE system failed with rc=#{rc}" if rc != 0
-end
+raise "Loading SQLAllocHandle failed" if rc != 1
+handle = SQLhandle.malloc
+ILEarguments = ILEarglist.malloc
+ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
+ILEarguments[ 32,  2] = ['0001'].pack("H*")             # htype (SQL_HANDLE_ENV)
+ILEarguments[ 34,  2] = ['0000'].pack("H*")             # padding
+ILEarguments[ 36,  4] = ['00000000'].pack("H*")         # ihandle (SQL_NULL_HANDLE)
+ILEarguments[ 40,  8] = ['0000000000000000'].pack("H*") # padding
+ILEarguments[ 48, 16] = [handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
+rc = ilecallx.call(ILEfunction, ILEarguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
+raise "ILE system failed with rc=#{rc}" if rc != 0
 puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
 puts ILEarguments[  0, 16].unpack("H*")
 puts ILEarguments[ 16, 16].unpack("H*")
@@ -38,3 +37,18 @@ puts ILEarguments[ 32, 16].unpack("H*")
 puts ILEarguments[ 48, 16].unpack("H*")
 puts ' 0 1 2 3'
 puts handle[ 0, 4].unpack("H*")
+ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
+ILEarguments[ 32,  2] = ['0002'].pack("H*")             # htype (SQL_HANDLE_DBC)
+ILEarguments[ 34,  2] = ['0000'].pack("H*")             # padding
+ILEarguments[ 36,  4] = handle[ 0, 4]                   # ihandle
+ILEarguments[ 40,  8] = ['0000000000000000'].pack("H*") # padding
+ILEarguments[ 48, 16] = [handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
+rc = ilecallx.call(ILEfunction, ILEarguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
+raise "ILE system failed with rc=#{rc}" if rc != 0
+puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
+puts ILEarguments[  0, 16].unpack("H*")
+puts ILEarguments[ 16, 16].unpack("H*")
+puts ILEarguments[ 32, 16].unpack("H*")
+puts ILEarguments[ 48, 16].unpack("H*")
+puts ' 0 1 2 3'
+
