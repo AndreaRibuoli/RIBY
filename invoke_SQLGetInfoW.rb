5e04422c584186da4ccbbbf1f999a63d40bf75d8
Andrea Ribuoli <andrea@powerruby.com>
2021-04-03 21:22:44 +0200

    21

--- invoke_SQLGetInfoW.rb
+++ invoke_SQLGetInfoW.rb
@@ -38,6 +38,11 @@ ILEarguments[ 40,  8] = ['0000000000000000'].pack("H*") # padding
 ILEarguments[ 48, 16] = [env_handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
 rc = ilecallx.call(pSQLAllocHandle, ILEarguments, ['FFFDFFF9FFF50000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
+puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
+puts ILEarguments[   0, 16].unpack("H*")
+puts ILEarguments[  16, 16].unpack("H*")
+puts ILEarguments[  32, 16].unpack("H*")
+puts ILEarguments[  48, 16].unpack("H*")
 puts 'Environment handle 0x' + env_handle[ 0, 8].unpack("H*")[0]
 dbc_handle = SQLhandle.malloc
 ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
@@ -47,6 +52,11 @@ ILEarguments[ 40,  8] = env_handle[ 0, 8]               # ihandle
 ILEarguments[ 48, 16] = [dbc_handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
 rc = ilecallx.call(pSQLAllocHandle, ILEarguments, ['FFFDFFF9FFF50000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
+puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
+puts ILEarguments[   0, 16].unpack("H*")
+puts ILEarguments[  16, 16].unpack("H*")
+puts ILEarguments[  32, 16].unpack("H*")
+puts ILEarguments[  48, 16].unpack("H*")
 puts 'DB Connection handle 0x' + dbc_handle[ 0, 8].unpack("H*")[0]
 dsn = '*LOCAL'.encode('UTF-16BE')
 ILEarguments[   0, 32] = ['0'.rjust(64,'0')].pack("H*")
@@ -65,6 +75,13 @@ ILEarguments[ 128,  2] = ['FFFD'].pack("H*")             # SQL_NTS
 ILEarguments[ 130, 14] = ['0'.rjust(28,'0')].pack("H*")  # padding
 rc = ilecallx.call(pSQLConnectW, ILEarguments, ['FFFBFFF5FFFDFFF5FFFDFFF5FFFD0000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
+puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
+puts ILEarguments[   0, 16].unpack("H*")
+puts ILEarguments[  16, 16].unpack("H*")
+puts ILEarguments[  32, 16].unpack("H*")
+puts ILEarguments[  48, 16].unpack("H*")
+puts ILEarguments[  64, 16].unpack("H*")
+puts ILEarguments[  80, 16].unpack("H*")
 size   = SQLretsize.malloc
 buffer = INFObuffer.malloc
 ILEarguments[   0, 32] = ['0'.rjust(64,'0')].pack("H*")
