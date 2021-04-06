9d119b9078f6408eedaa4b1b4801b2dfd99d091b
Andrea Ribuoli <andrea@powerruby.com>
2021-04-06 16:47:18 +0200

    23

--- invoke_SQLSetConnectAttrW.rb
+++ invoke_SQLSetConnectAttrW.rb
@@ -79,7 +79,7 @@ ILEarguments[  36,  4] = [ 10029.to_s(16).rjust(8,'0')].pack("H*")
 ILEarguments[  40,  4] = [   255.to_s(16).rjust(8,'0')].pack("H*")
 ILEarguments[  44,  4] = [     0.to_s(16).rjust(8,'0')].pack("H*")
 ILEarguments[  48, 96] = ['0'.rjust(192,'0')].pack("H*")  # padding
-rc = ilecallx.call(pSQLGetConnectAttrW, ILEarguments, ['FFFBFFFBFFF5FFFB0000'].pack("H*"), -5, 0)
+rc = ilecallx.call(pSQLSetConnectAttrW, ILEarguments, ['FFFBFFFBFFF5FFFB0000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
 puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
 puts ILEarguments[   0, 16].unpack("H*")
