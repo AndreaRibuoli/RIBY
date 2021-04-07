efcda91db382f71fe8a50a554c5d5ea45bd58a8a
Andrea Ribuoli <andrea@powerruby.com>
2021-04-07 07:57:47 +0200

    23

--- invoke_SQLGetStmtAttrW.rb
+++ invoke_SQLGetStmtAttrW.rb
@@ -7,6 +7,8 @@ raise "Usage: invoke_SQLAllocHandle.rb" if ARGV.length != 0
 ILEpointer  = struct [ 'char b[16]' ]
 ILEarglist  = struct [ 'char c[144]' ]
 SQLhandle   = struct [ 'char a[4]' ]
+INFObuffer  = struct [ 'char i[4096]' ]
+SQLintsize  = struct [ 'char s[4]' ]
 preload    = Fiddle.dlopen(nil)
 ileloadx   = Fiddle::Function.new( preload['_ILELOADX'],
                            [Fiddle::TYPE_VOIDP, Fiddle::TYPE_INT],
@@ -24,6 +26,9 @@ raise "Loading SQLAllocHandle failed" if rc != 1
 pSQLConnectW = ILEpointer.malloc
 rc = ilesymx.call(pSQLConnectW, qsqcli, 'SQLConnectW')
 raise "Loading SQLConnectW failed" if rc != 1
+pSQLGetStmtAttrW = ILEpointer.malloc
+rc = ilesymx.call(pSQLGetStmtAttrW, qsqcli, 'SQLGetStmtAttrW')
+raise "Loading SQLGetStmtAttrW failed" if rc != 1
 env_handle = SQLhandle.malloc
 ILEarguments = ILEarglist.malloc
 ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
@@ -62,33 +67,45 @@ ILEarguments[ 128,  2] = ['FFFD'].pack("H*")             # SQL_NTS
 ILEarguments[ 130, 14] = ['0'.rjust(28,'0')].pack("H*")  # padding
 rc = ilecallx.call(pSQLConnectW, ILEarguments, ['FFFBFFF5FFFDFFF5FFFDFFF5FFFD0000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
-stm1_handle = SQLhandle.malloc
+stm_handle = SQLhandle.malloc
 ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
 ILEarguments[ 32,  2] = ['0003'].pack("H*")             # htype (SQL_HANDLE_STM)
 ILEarguments[ 34,  2] = ['0000'].pack("H*")             # padding
 ILEarguments[ 36,  4] = dbc_handle[ 0, 4]               # ihandle
 ILEarguments[ 40,  8] = ['0000000000000000'].pack("H*") # padding
-ILEarguments[ 48, 16] = [stm1_handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
+ILEarguments[ 48, 16] = [stm_handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
 rc = ilecallx.call(pSQLAllocHandle, ILEarguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
-puts 'Statement handle 0x' + stm1_handle[ 0, 4].unpack("H*")[0]
-stm2_handle = SQLhandle.malloc
-ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
-ILEarguments[ 32,  2] = ['0003'].pack("H*")             # htype (SQL_HANDLE_STM)
-ILEarguments[ 34,  2] = ['0000'].pack("H*")             # padding
-ILEarguments[ 36,  4] = dbc_handle[ 0, 4]               # ihandle
-ILEarguments[ 40,  8] = ['0000000000000000'].pack("H*") # padding
-ILEarguments[ 48, 16] = [stm2_handle.to_i.to_s(16).rjust(32,'0')].pack("H*")
-rc = ilecallx.call(pSQLAllocHandle, ILEarguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
-raise "ILE system failed with rc=#{rc}" if rc != 0
-puts 'Statement handle 0x' + stm2_handle[ 0, 4].unpack("H*")[0]
-puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
-puts ILEarguments[   0, 16].unpack("H*")
-puts ILEarguments[  16, 16].unpack("H*")
-puts ILEarguments[  32, 16].unpack("H*")
-puts ILEarguments[  48, 16].unpack("H*")
-puts ILEarguments[  64, 16].unpack("H*")
-puts ILEarguments[  80, 16].unpack("H*")
-puts ILEarguments[  96, 16].unpack("H*")
-puts ILEarguments[ 112, 16].unpack("H*")
-puts ILEarguments[ 128, 16].unpack("H*")
+puts 'Statement handle 0x' + stm_handle[ 0, 4].unpack("H*")[0]
+sizeint = SQLintsize.malloc
+buffer  = INFObuffer.malloc
+ILEarguments[  32,  4] = stm_handle[ 0, 4]               # stmt
+ILEarguments[  40,  8] = ['0'.rjust(16,'0')].pack("H*")  # padding
+ILEarguments[  64,  4] = ['00001000'].pack("H*")         # 4
+ILEarguments[  68, 76] = ['0'.rjust(152,'0')].pack("H*")  # padding
+working = []
+12000.times { |key|
+  ILEarguments[   0, 32] = ['0'.rjust(64,'0')].pack("H*")
+  ILEarguments[  36,  4] = [key.to_s(16).rjust(8,'0')].pack("H*")
+  ILEarguments[  48, 16] = [buffer.to_i.to_s(16).rjust(32,'0')].pack("H*")
+  ILEarguments[  80, 16] = [sizeint.to_i.to_s(16).rjust(32,'0')].pack("H*")
+  buffer[0, 4] = ['00000000'].pack("H*")
+  rc = ilecallx.call(pSQLGetStmtAttrW, ILEarguments, ['FFFBFFFBFFF5FFFBFFF50000'].pack("H*"), -5, 0)
+  raise "ILE system failed with rc=#{rc}" if rc != 0
+  working.push(key) if ILEarguments[16, 8].unpack("H*")[0] != 'ffffffffffffffff'
+}
+{
+  SQL_ATTR_DECFLOAT_ROUNDING_MODE: 10112
+}.each { |k,key|
+  ILEarguments[   0, 32] = ['0'.rjust(64,'0')].pack("H*")
+  ILEarguments[  36,  4] = [key.to_s(16).rjust(8,'0')].pack("H*")
+  ILEarguments[  48, 16] = [buffer.to_i.to_s(16).rjust(32,'0')].pack("H*")
+  ILEarguments[  80, 16] = [sizeint.to_i.to_s(16).rjust(32,'0')].pack("H*")
+  buffer[0, 4] = ['00000000'].pack("H*")
+  rc = ilecallx.call(pSQLGetStmtAttrW, ILEarguments, ['FFFBFFFBFFF5FFFBFFF50000'].pack("H*"), -5, 0)
+  working.delete(key) if ILEarguments[16, 8].unpack("H*")[0] != 'ffffffffffffffff'
+  puts "#{k.to_s} (#{key}): 0x#{buffer[0, 4].unpack("H*")[0]}"
+}
+working.each {|key|
+  puts "Attribute #{key} unknown"
+}
