daf2ee61c837437b5881d1f9599ae49c9e0bc6e6
Andrea Ribuoli <andrea@powerruby.com>
2021-04-08 16:25:57 +0200

    25

--- invoke_SQLConnectW_wErrorMsg.rb
+++ invoke_SQLConnectW_wErrorMsg.rb
@@ -7,6 +7,10 @@ raise "Usage: invoke_SQLAllocHandle.rb <dsn> <user> <pass>" if ARGV.length != 3
 ILEpointer  = struct [ 'char b[16]' ]
 ILEarglist  = struct [ 'char c[144]' ]
 SQLhandle   = struct [ 'char a[4]' ]
+SQLerror    = struct [ 'char e[4]' ]
+SQLstate    = struct [ 'char s[12]' ]
+SQLmsg      = struct [ 'char s[1026]' ]
+SQLmsglen   = struct [ 'char l[2]' ]
 preload    = Fiddle.dlopen(nil)
 ileloadx   = Fiddle::Function.new( preload['_ILELOADX'],
                            [Fiddle::TYPE_VOIDP, Fiddle::TYPE_INT],
@@ -24,6 +28,9 @@ raise "Loading SQLAllocHandle failed" if rc != 1
 pSQLConnectW = ILEpointer.malloc
 rc = ilesymx.call(pSQLConnectW, qsqcli, 'SQLConnectW')
 raise "Loading SQLConnectW failed" if rc != 1
+pSQLErrorW = ILEpointer.malloc
+rc = ilesymx.call(pSQLErrorW, qsqcli, 'SQLErrorW')
+raise "Loading SQLErrorW failed" if rc != 1
 env_handle = SQLhandle.malloc
 ILEarguments = ILEarglist.malloc
 ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
@@ -72,3 +79,30 @@ puts ILEarguments[  80, 16].unpack("H*")
 puts ILEarguments[  96, 16].unpack("H*")
 puts ILEarguments[ 112, 16].unpack("H*")
 puts ILEarguments[ 128, 16].unpack("H*")
+state  = SQLstate.malloc
+error  = SQLerror.malloc
+msg    = SQLmsg.malloc
+msglen = SQLmsg.malloc
+ILEarguments[   0, 32] = ['0'.rjust(64,'0')].pack("H*")
+ILEarguments[  32,  4] = env_handle[ 0, 4]               # henv
+ILEarguments[  36,  4] = dbc_handle[ 0, 4]               # hdbc
+ILEarguments[  40,  8] = ['0'.rjust(16,'0')].pack("H*")  # padding
+ILEarguments[  48, 16] = [state.to_i.to_s(16).rjust(32,'0')].pack("H*")
+ILEarguments[  64, 16] = [error.to_i.to_s(16).rjust(32,'0')].pack("H*")
+ILEarguments[  80, 16] = [msg.to_i.to_s(16).rjust(32,'0')].pack("H*")
+ILEarguments[  96,  2] = ['0402'].pack("H*")             # SQL_NTS
+ILEarguments[  98, 14] = ['0'.rjust(28,'0')].pack("H*")  # padding
+ILEarguments[ 112, 16] = [msglen.to_i.to_s(16).rjust(32,'0')].pack("H*")
+rc = ilecallx.call(pSQLErrorW, ILEarguments, ['FFFBFFFBFFFBFFF5FFF5FFF5FFFDFFF50000'].pack("H*"), -5, 0)
+raise "ILE system failed with rc=#{rc}" if rc != 0
+puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
+puts ILEarguments[   0, 16].unpack("H*")
+puts ILEarguments[  16, 16].unpack("H*")
+puts ILEarguments[  32, 16].unpack("H*")
+puts ILEarguments[  48, 16].unpack("H*")
+puts ILEarguments[  64, 16].unpack("H*")
+puts ILEarguments[  80, 16].unpack("H*")
+puts ILEarguments[  96, 16].unpack("H*")
+puts ILEarguments[ 112, 16].unpack("H*")
+puts ILEarguments[ 128, 16].unpack("H*")
+
