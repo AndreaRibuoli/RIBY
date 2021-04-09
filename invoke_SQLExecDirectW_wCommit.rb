73c8fd211a9bfb32f9bedf136f946951ba8850f1
Andrea Ribuoli <andrea@powerruby.com>
2021-04-09 10:30:50 +0200

    26

--- invoke_SQLExecDirectW_wCommit.rb
+++ invoke_SQLExecDirectW_wCommit.rb
@@ -133,11 +133,11 @@ MSG=#{msg[0, l].force_encoding('UTF-16BE').encode('utf-8')}
 END_HERE
 puts final if (exec_rc != 0) || (l>0)
 ILEarguments[   0, 32] = ['0'.rjust(64,'0')].pack("H*")
-ILEarguments[  32,  4] = ['0002'].pack("H*")              # SQL_HANDLE_ENV
-ILEarguments[  34,  6] = ['0'.rjust(12,'0')].pack("H*")   # padding
-ILEarguments[  40,  4] = dbc_handle[ 0, 4]                # hdbc
-ILEarguments[  44, 100] = ['0'.rjust(200,'0')].pack("H*") # padding
-rc = ilecallx.call(pSQLErrorW, ILEarguments, ['FFFDFFF9FFFD0000'].pack("H*"), -5, 0)
+ILEarguments[  32,  4] = ['0002'].pack("H*")              # SQL_HANDLE_DBC
+ILEarguments[  34,  2] = ['0'.rjust(12,'0')].pack("H*")   # padding
+ILEarguments[  36,  4] = dbc_handle[ 0, 4]                # hdbc
+ILEarguments[  40, 104] = ['0'.rjust(208,'0')].pack("H*") # padding
+rc = ilecallx.call(pSQLErrorW, ILEarguments, ['FFFDFFFBFFFD0000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
 puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
 puts ILEarguments[   0, 16].unpack("H*")
