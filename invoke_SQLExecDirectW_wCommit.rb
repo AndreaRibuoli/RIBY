043e65c3879b8a1b3924c6dc2ca8a7337f7078e9
Andrea Ribuoli <andrea@powerruby.com>
2021-04-09 18:53:13 +0200

    27

--- invoke_SQLExecDirectW_wCommit.rb
+++ invoke_SQLExecDirectW_wCommit.rb
@@ -141,17 +141,6 @@ ILEarguments[  34,  2] = ['0'.rjust(12,'0')].pack("H*")   # padding
 ILEarguments[  36,  4] = dbc_handle[ 0, 4]                # hdbc
 ILEarguments[  40, 104] = ['0'.rjust(208,'0')].pack("H*") # padding
 rc = ilecallx.call(pSQLEndTran, ILEarguments, ['FFFDFFFBFFFD0000'].pack("H*"), -5, 0)
-raise "ILE system failed with rc=#{rc}" if rc != 0
-puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
-puts ILEarguments[   0, 16].unpack("H*")
-puts ILEarguments[  16, 16].unpack("H*")
-puts ILEarguments[  32, 16].unpack("H*")
-puts ILEarguments[  48, 16].unpack("H*")
-puts ILEarguments[  64, 16].unpack("H*")
-puts ILEarguments[  80, 16].unpack("H*")
-puts ILEarguments[  96, 16].unpack("H*")
-puts ILEarguments[ 112, 16].unpack("H*")
-puts ILEarguments[ 128, 16].unpack("H*")
 ILEarguments[   0, 32] = ['0'.rjust(64,'0')].pack("H*")
 ILEarguments[  32,  4] = env_handle[ 0, 4]               # henv
 ILEarguments[  36,  4] = dbc_handle[ 0, 4]               # hdbc
@@ -164,16 +153,6 @@ ILEarguments[  98, 14] = ['0'.rjust(28,'0')].pack("H*")  # padding
 ILEarguments[ 112, 16] = [msglen.to_i.to_s(16).rjust(32,'0')].pack("H*")
 rc = ilecallx.call(pSQLErrorW, ILEarguments, ['FFFBFFFBFFFBFFF5FFF5FFF5FFFDFFF50000'].pack("H*"), -5, 0)
 raise "ILE system failed with rc=#{rc}" if rc != 0
-puts ' 0 1 2 3 4 5 6 7 8 9 A B C D E F'
-puts ILEarguments[   0, 16].unpack("H*")
-puts ILEarguments[  16, 16].unpack("H*")
-puts ILEarguments[  32, 16].unpack("H*")
-puts ILEarguments[  48, 16].unpack("H*")
-puts ILEarguments[  64, 16].unpack("H*")
-puts ILEarguments[  80, 16].unpack("H*")
-puts ILEarguments[  96, 16].unpack("H*")
-puts ILEarguments[ 112, 16].unpack("H*")
-puts ILEarguments[ 128, 16].unpack("H*")
 l = msglen[0, 2].unpack("H*")[0].to_i(16) * 2
 final2 =<<END_HERE2
 RC=#{exec_rc};
