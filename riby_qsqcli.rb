feca0efdfa268dba6ad093a95a02f14a69ddbf0e
Andrea Ribuoli <andrea@powerruby.com>
2021-04-11 23:37:31 +0200

    28

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -24,9 +24,9 @@ module RibyCli
   def SQLAllocHandle(htype, ihandle, handle)
     ileArguments = ILEarglist.malloc
     ileArguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
-    ileArguments[ 32,  2] = [htype.to_i(16).rjust(4,'0')].pack("H*")
+    ileArguments[ 32,  2] = [htype.to_s(16).rjust(4,'0')].pack("H*")
     ileArguments[ 34,  2] = ['0000'].pack("H*")
-    ileArguments[ 36,  4] = [ihandle.to_i(16).rjust(8,'0')].pack("H*")
+    ileArguments[ 36,  4] = [ihandle.to_s(16).rjust(8,'0')].pack("H*")
     ileArguments[ 40, 24] = [handle.to_i.to_s(16).rjust(48,'0')].pack("H*")
     rc = ilecallx.call(P_SQLAllocHandle, ileArguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
   end
