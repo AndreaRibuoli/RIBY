2c28f2c17f726439251e699655a8dd2b971a0db2
Andrea Ribuoli <andrea@powerruby.com>
2021-04-11 23:03:05 +0200

    28

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -61,13 +61,13 @@ module RibyCli
   end
   
   def SQLAllocHandle(htype, ihandle, handle)
-    ILEarguments = ILEarglist.malloc
-    ILEarguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
-    ILEarguments[ 32,  2] = [htype.to_i(16).rjust(4,'0')].pack("H*")
-    ILEarguments[ 34,  2] = ['0000'].pack("H*")
-    ILEarguments[ 36,  4] = [ihandle.to_i(16).rjust(8,'0')].pack("H*")
-    ILEarguments[ 40, 24] = [handle.to_i.to_s(16).rjust(48,'0')].pack("H*")
-    rc = ilecallx.call(pfSQLAllocHandle, ILEarguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
+    ileArguments = ILEarglist.malloc
+    ileArguments[  0, 32] = ['0'.rjust(64,'0')].pack("H*")
+    ileArguments[ 32,  2] = [htype.to_i(16).rjust(4,'0')].pack("H*")
+    ileArguments[ 34,  2] = ['0000'].pack("H*")
+    ileArguments[ 36,  4] = [ihandle.to_i(16).rjust(8,'0')].pack("H*")
+    ileArguments[ 40, 24] = [handle.to_i.to_s(16).rjust(48,'0')].pack("H*")
+    rc = ilecallx.call(pfSQLAllocHandle, ileArguments, ['FFFDFFFBFFF50000'].pack("H*"), -5, 0)
   end
 
 end
