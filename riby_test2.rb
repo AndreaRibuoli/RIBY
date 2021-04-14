a915fc222356bbf12fe05cf87136199eee7bda9e
Andrea Ribuoli <andrea@powerruby.com>
2021-04-14 23:52:24 +0200

    30

--- riby_test2.rb
+++ riby_test2.rb
@@ -11,10 +11,7 @@ h.attrs=({:SQL_ATTR_DATE_FMT    => :SQL_FMT_EUR,
          })
 d = Connect.new(h, '*LOCAL')
 d.Empower(ARGV[0],ARGV[1])
-d.attrs=({:SQL_ATTR_DBC_DEFAULT_LIB => "PROVA     ",
-          :SQL_ATTR_SAVEPOINT_NAME  => "ADESSO"    ,
-          :SQL_ATTR_TIME_FMT        => :SQL_FMT_EUR
-         })
+d.attrs=({SQL_ATTR_TIME_FMT      => :SQL_FMT_EUR})
 s = Stmt.new(d)
 s.attrs=({:SQL_ATTR_FOR_FETCH_ONLY => :SQL_TRUE})
 pp h.attrs
