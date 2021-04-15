857b1adf2d254bd66d1f047c59027bcd40d082ee
Andrea Ribuoli <andrea@powerruby.com>
2021-04-15 17:35:47 +0200

    30

---
+++ riby_test3.rb
@@ -0,0 +1,22 @@
+#! /QOpenSys/pkgs/bin/ruby
+require_relative 'riby_qsqcli'
+require 'pp'
+
+raise "Usage: #{__FILE__} <user> <password>" if ARGV.length != 2
+
+h = Env.new
+h.attrs=({:SQL_ATTR_DATE_FMT    => :SQL_FMT_EUR,
+          :SQL_ATTR_ESCAPE_CHAR => '@',
+          :SQL_ATTR_DEFAULT_LIB => 'INTELLIGEN'
+         })
+d = Connect.new(h, '*LOCAL')
+d.Empower(ARGV[0],ARGV[1])
+d.attrs=({:SQL_ATTR_TIME_FMT        => :SQL_FMT_EUR,
+          :SQL_ATTR_DBC_DEFAULT_LIB => 'PROVAPIENA',
+          :SQL_ATTR_MAX_PRECISION   => 63
+         })
+s = Stmt.new(d)
+s.attrs=({:SQL_ATTR_FOR_FETCH_ONLY => :SQL_TRUE})
+pp h.attrs
+pp d.attrs
+pp s.attrs
