9f26f1aeb17853af6d8ee045c7c307ae9dfca954
Andrea Ribuoli <andrea@powerruby.com>
2021-04-15 11:32:21 +0200

    30

--- riby_test2.rb
+++ riby_test2.rb
@@ -6,8 +6,9 @@ raise "Usage: invoke_SQLGetConnectAttrW.rb <user> <password>" if ARGV.length != 
 
 h = Env.new
 h.attrs=({:SQL_ATTR_DATE_FMT    => :SQL_FMT_EUR,
-          :SQL_ATTR_DEFAULT_LIB => 'INTELLIGEN',
+          :SQL_ATTR_INCLUDE_NULL_IN_LEN => :SQL_FALSE,
           :SQL_ATTR_ESCAPE_CHAR => '@'
+          :SQL_ATTR_DEFAULT_LIB => 'INTELLIGEN',
          })
 d = Connect.new(h, '*LOCAL')
 d.Empower(ARGV[0],ARGV[1])
