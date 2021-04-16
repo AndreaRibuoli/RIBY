3b4d56233e16e16d6cbe76b2bf08799b5cf91e68
Andrea Ribuoli <andrea@powerruby.com>
2021-04-16 08:33:05 +0200

    31

--- riby_test4.rb
+++ riby_test4.rb
@@ -10,12 +10,12 @@ d1 = Connect.new(h, '*LOCAL')
 d1.Empower(ARGV[0],ARGV[1])
 s1 = Stmt.new(d1)
 puts s1.handle.unpack('l')
-begin
+5.times {
   d2 = Connect.new(h, '*LOCAL')
   d2.Empower(ARGV[0],ARGV[1])
   s2 = Stmt.new(d2)
   puts s2.handle.unpack('l')
-end
+}
 GC.start
 s3 = Stmt.new(d1)
 puts s3.handle.unpack('l')
