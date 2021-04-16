828a52b239573a82e93b32ef0eb1b138cf8932c1
Andrea Ribuoli <andrea@powerruby.com>
2021-04-16 21:54:40 +0200

    31

--- riby_test5.rb
+++ riby_test5.rb
@@ -11,10 +11,11 @@ d1.Empower(ARGV[0],ARGV[1])
 s1 =Stmt.new(d1)
 s2 =Stmt.new(d1)
 GC.stress = true
-10.times {
+5.times {
   di = Connect.new(h, '*LOCAL')
   di.Empower(ARGV[0],ARGV[1])
   Stmt.new(di)
+  puts "Connect #{di.handle.unpack('l')[0]}: #{di.jobname}"
 }
 s3 =Stmt.new(d1)
 puts "Statements #{s1.handle.unpack('l')[0]}, #{s2.handle.unpack('l')[0]} and #{s3.handle.unpack('l')[0]} are still allocated"
