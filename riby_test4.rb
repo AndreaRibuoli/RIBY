23d8c94f4eeda2e90ca56cb67fcc46436d25227b
Andrea Ribuoli <andrea@powerruby.com>
2021-04-16 12:14:15 +0200

    31

--- riby_test4.rb
+++ riby_test4.rb
@@ -5,19 +5,22 @@ require 'pp'
 raise "Usage: #{__FILE__} <user> <password>" if ARGV.length != 2
 
 h = Env.new
+puts "Env #{h.handle.unpack('l')[0]}"
 h.attrs=({:SQL_ATTR_SERVER_MODE => :SQL_TRUE})
 d1 = Connect.new(h, '*LOCAL')
+puts "Connect #{d1.handle.unpack('l')[0]}"
 d1.Empower(ARGV[0],ARGV[1])
 s1 = Stmt.new(d1)
-puts s1.handle.unpack('l')
+puts "Stmt #{s1.handle.unpack('l')[0]}"
 s2 = Stmt.new(d1)
-puts s2.handle.unpack('l')
+puts "Stmt #{s2.handle.unpack('l')[0]}"
 GC.stress = true
 20.times {
   di = Connect.new(h, '*LOCAL')
+  puts "Connect #{di.handle.unpack('l')[0]}"
   di.Empower(ARGV[0],ARGV[1])
   si = Stmt.new(di)
-  puts si.handle.unpack('l')
+  puts "Stmt #{si.handle.unpack('l')[0]}"
 }
 s3 = Stmt.new(d1)
-puts s3.handle.unpack('l')
+puts "Stmt #{s3.handle.unpack('l')[0]}"
