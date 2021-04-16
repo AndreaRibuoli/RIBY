4ca0935469606294b53f1179fb170de1d0379396
Andrea Ribuoli <andrea@powerruby.com>
2021-04-16 19:51:34 +0200

    31

--- riby_test6.rb
+++ riby_test6.rb
@@ -4,19 +4,11 @@ require 'pp'
 
 raise "Usage: #{__FILE__} <user> <password>" if ARGV.length != 2
 
-h = Env.new
-h.attrs=({:SQL_ATTR_SERVER_MODE => :SQL_TRUE})
-d1 = Connect.new(h, '*LOCAL')
-d1.Empower(ARGV[0],ARGV[1])
-s1 =Stmt.new(d1)
-s2 =Stmt.new(d1)
-puts "DB Connect #{d1.handle.unpack('l')[0]}: #{d1.jobname}"
-GC.stress = true
-50.times {
+15.times {
+  h = Env.new
+  h.attrs=({:SQL_ATTR_SERVER_MODE => :SQL_TRUE})
   di = Connect.new(h, '*LOCAL')
   di.Empower(ARGV[0],ARGV[1])
   Stmt.new(di)
   puts "DB Connect #{di.handle.unpack('l')[0]}: #{di.jobname}"
 }
-s3 =Stmt.new(d1)
-puts "Statements #{s1.handle.unpack('l')[0]}, #{s2.handle.unpack('l')[0]} and #{s3.handle.unpack('l')[0]} are still allocated"
