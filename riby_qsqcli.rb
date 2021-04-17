5a5d87d7a72f8763f2041076cabbca83deb0d313
Andrea Ribuoli <andrea@powerruby.com>
2021-04-17 12:15:38 +0200

    31

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -418,7 +418,6 @@ class Connect
     ileArguments[   0,  32] = ['0'.rjust(64,'0')].pack("H*")
     ileArguments[  32,   4] = handle                          # hdbc
     ileArguments[  36, 108] = ['0'.rjust(216,'0')].pack("H*")
-    puts P_Disconnect
     rc = Ilecallx.call(P_Disconnect, ileArguments, ['FFFB0000'].pack("H*"), -5, 0)
   end
 end
