7fdeb8e54463a99fb9eb9536740fbae3428ac675
Andrea Ribuoli <andrea@powerruby.com>
2021-04-19 11:02:49 +0200

    31

--- riby_test8.rb
+++ riby_test8.rb
@@ -11,6 +11,6 @@ h.attrs=({:SQL_ATTR_SERVER_MODE => :SQL_TRUE})
   di = Connect.new(h, '*LOCAL')
   di.Empower(ARGV[0],ARGV[1])
   Stmt.new(di)
-  puts "#{di.handle.unpack('H*')} #{'%10.7f' % Time.now.to_f} #{di.jobname}"
+  puts "#{di.handle.unpack('H*')} #{'%10.7f' % Time.now.to_f} Connect: #{di.jobname}"
   di.disconnect
 }                                                                      
