d7505004488f1d0fc65a1fcaecec12dcf637b45f
Andrea Ribuoli <andrea@powerruby.com>
2021-04-19 14:48:58 +0200

    31

--- riby_test9.rb
+++ riby_test9.rb
@@ -9,7 +9,8 @@ h = Env.new
 h.attrs=({:SQL_ATTR_SERVER_MODE => :SQL_TRUE,
          :SQL_ATTR_SERVERMODE_SUBSYSTEM => "RIBY_SBSD"
          })
-100.times {
+GC.stress = true
+20.times {
   di = Connect.new(h, '*LOCAL')
   di.Empower(ARGV[0],ARGV[1])
   Stmt.new(di)
