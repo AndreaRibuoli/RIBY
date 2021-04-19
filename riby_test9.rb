11894512391b4ffcbb9b08a73e5cbfe1ef27a649
Andrea Ribuoli <andrea@powerruby.com>
2021-04-19 14:02:56 +0200

    31

---
+++ riby_test9.rb
@@ -0,0 +1,18 @@
+#! /QOpenSys/pkgs/bin/ruby
+require_relative 'riby_qsqcli'
+require 'pp'
+                                                                       
+raise "Usage: #{__FILE__} <user> <password>" if ARGV.length != 2
+                                                                       
+# GC.stress = true
+h = Env.new
+h.attrs=({:SQL_ATTR_SERVER_MODE => :SQL_TRUE,
+         :SQL_ATTR_SERVERMODE_SUBSYSTEM => "*SAME"
+         })
+100.times {
+  di = Connect.new(h, '*LOCAL')
+  di.Empower(ARGV[0],ARGV[1])
+  Stmt.new(di)
+  puts "#{di.handle.unpack('H*')} #{'%10.7f' % Time.now.to_f} Connect: #{di.jobname}"
+  di.disconnect
+}                                                                      
