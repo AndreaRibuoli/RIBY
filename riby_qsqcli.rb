db2eb90a9a872e2ff7bded961e05154120ec1f03
Andrea Ribuoli <andrea@powerruby.com>
2021-04-20 09:21:44 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -174,14 +174,6 @@ module RibyCli
     return ileArguments[ 16, 4].unpack('l')[0]
   end
   
-  def self.SQLReleaseEnv(handle)
-    ileArguments = ILEarglist.malloc
-    ileArguments[   0,  32] = ['0'.rjust(64,'0')].pack("H*")
-    ileArguments[  32,   4] = handle                          # henv
-    ileArguments[  36, 108] = ['0'.rjust(216,'0')].pack("H*")
-    Ilecallx.call(SQLApis['SQLReleaseEnv'], ileArguments, SQLApiList['SQLReleaseEnv'], - 5, 0)
-    return ileArguments[ 16, 4].unpack('l')[0]
-  end
   
   def self.SQLDisconnect(handle)
     ileArguments = ILEarglist.malloc
@@ -208,8 +200,8 @@ class Env
   end
   def self.finalizer_proc(h)
     proc {
-      rc = RibyCli::SQLRelaseEnv(h)
- #     puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Release Env (#{rc})"  if $-W >= 2
+      rc = Env::SQLRelaseEnv(h)
+      puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Release Env (#{rc})"  if $-W >= 2
       rc = RibyCli::SQLFreeHandle(SQL_HANDLE_ENV, h)
       puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Free Env (#{rc})" if $-W >= 2
     }
@@ -344,6 +336,14 @@ class Env
       ileArguments[  40,  8] = ['0'.rjust(16,'0')].pack("H*")   # padding
       ileArguments[  68, 76] = ['0'.rjust(152,'0')].pack("H*")  # padding
       Ilecallx.call(SQLApis['SQLSetEnvAttr'], ileArguments, SQLApiList['SQLSetEnvAttr'], - 5, 0)
+      return ileArguments[ 16, 4].unpack('l')[0]
+    end
+    def self.SQLReleaseEnv(henv)
+      ileArguments = ILEarglist.malloc
+      ileArguments[   0,  32] = ['0'.rjust(64,'0')].pack("H*")
+      ileArguments[  32,   4] = henv                             # henv
+      ileArguments[  36, 108] = ['0'.rjust(216,'0')].pack("H*")
+      Ilecallx.call(SQLApis['SQLReleaseEnv'], ileArguments, SQLApiList['SQLReleaseEnv'], - 5, 0)
       return ileArguments[ 16, 4].unpack('l')[0]
     end
 #    def SQLReleaseEnv
