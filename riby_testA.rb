e1fa775cf74c5cbc27507531ff76b5da7faba61a
Andrea Ribuoli <andrea@powerruby.com>
2021-04-20 08:38:20 +0200

    33

--- riby_testA.rb
+++ riby_testA.rb
@@ -7,5 +7,5 @@ raise "Usage: #{__FILE__} <user> <password>" if ARGV.length != 2
 e = Env.new
 e.attrs = { :SQL_ATTR_SERVER_MODE => :SQL_TRUE }
 c = Connect.new(e)
-c.empower(ARGV[0], ARGV[1])
+pp c.empower(ARGV[0], ARGV[1])
 pp c.error
