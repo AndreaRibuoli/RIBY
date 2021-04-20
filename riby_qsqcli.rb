62839b522ea7609bcf8df112078c8e4eaa833595
Andrea Ribuoli <andrea@powerruby.com>
2021-04-20 10:17:42 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -175,6 +175,15 @@ module RibyCli
     return ileArguments[ 16, 4].unpack('l')[0]
   end
   
+  def self.SQLDisconnect(hdbc)
+    ileArguments = ILEarglist.malloc
+    ileArguments[   0,  32] = ['0'.rjust(64,'0')].pack("H*")
+    ileArguments[  32,   4] = hdbc
+    ileArguments[  36, 108] = ['0'.rjust(216,'0')].pack("H*")
+    Ilecallx.call(SQLApis['SQLDisconnect'], ileArguments, SQLApiList['SQLDisconnect'], - 5, 0)
+    return ileArguments[ 16, 4].unpack('l')[0]
+  end
+
 end
 
 class Env
@@ -365,7 +374,7 @@ class Connect
   end
   def self.finalizer_proc(h,henv)
     proc {
-      rc = Connect::SQLDisconnect(h)
+      rc = RibyCli::SQLDisconnect(h)
       puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Disconnect (#{rc})"  if $-W >= 2
       rc = RibyCli::SQLFreeHandle(SQL_HANDLE_DBC, h)
       puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Free Connect (#{rc})"  if $-W >= 2
@@ -469,14 +478,6 @@ class Connect
     # SQLDisconnect()
   end
 
-  def self.SQLDisconnect(hdbc)
-    ileArguments = ILEarglist.malloc
-    ileArguments[   0,  32] = ['0'.rjust(64,'0')].pack("H*")
-    ileArguments[  32,   4] = hdbc
-    ileArguments[  36, 108] = ['0'.rjust(216,'0')].pack("H*")
-    Ilecallx.call(SQLApis['SQLDisconnect'], ileArguments, SQLApiList['SQLDisconnect'], - 5, 0)
-    return ileArguments[ 16, 4].unpack('l')[0]
-  end
 
   private
   ATTRS = {
