363cb78dccddf0d8679832a70b69e5e37853712b
Andrea Ribuoli <andrea@powerruby.com>
2021-04-20 09:45:13 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -209,9 +209,9 @@ class Env
   end
   def self.finalizer_proc(h)
     proc {|h|
-      rc1 = RibyCli::SQLRelaseEnv(h)
+    #  rc1 = RibyCli::SQLRelaseEnv(h)
+    #  puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Release Env (#{rc1})"  if $-W >= 2
       rc2 = RibyCli::SQLFreeHandle(SQL_HANDLE_ENV, h)
-      puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Release Env (#{rc1})"  if $-W >= 2
       puts "#{h.unpack('H*')} #{'%10.7f' % Time.now.to_f} Free Env (#{rc2})" if $-W >= 2
     }
   end
