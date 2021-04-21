53e03ac68b1d314143f51c5c21613468836de100
Andrea Ribuoli <andrea@powerruby.com>
2021-04-21 21:37:55 +0200

    33

---
+++ riby_testC.rb
@@ -0,0 +1,14 @@
+#! /QOpenSys/pkgs/bin/ruby
+require_relative 'riby_qsqcli'
+require 'pp'
+                                                                       
+raise "Usage: #{__FILE__} <user> <password>" if ARGV.length != 2
+                                                                       
+e = Env.new
+e.attrs = { :SQL_ATTR_SERVER_MODE => :SQL_TRUE }
+c = Connect.new(e)
+pp c.empower(ARGV[0], ARGV[1])
+s = Stmt.new(c)
+Thread.new { s.cancel }
+pp s.error
+pp s.error
