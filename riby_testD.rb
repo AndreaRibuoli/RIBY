0a9219c5e9d63465ce563d0eacb940078b7f4556
Andrea Ribuoli <andrea@powerruby.com>
2021-04-28 11:14:05 +0200

    bind

--- riby_testD.rb
+++ riby_testD.rb
@@ -8,11 +8,10 @@ e = Env.new
 e.attrs = { :SQL_ATTR_SERVER_MODE => :SQL_TRUE }
 c = Connect.new(e)
 pp c.empower(ARGV[0], ARGV[1])
-#s2 = Stmt.new(c)
-#s2.primarykeys(ARGV[2], ARGV[3])
-#pp s2.error(1)
 s = Stmt.new(c)
-s.tables(ARGV[2], ARGV[3], ARGV[4])
+s.primarykeys(ARGV[2], ARGV[3])
+#s.tables(ARGV[2], ARGV[3], ARGV[4])
+#s.foreignkeys(ARGV[2], ARGV[3], '', '')
 n = s.columns_count[:SQL_DESC_COUNT]
 puts n 
 cols = []
@@ -21,10 +20,3 @@ cols.each { |f| f.bind }
 while s.fetch == 0
   cols.each { |f| pp f.buffer }
 end
-
-#s3 = Stmt.new(c)
-#s3.foreignkeys(ARGV[2], ARGV[3], '', '')
-#pp s3.error(1)
-#s4 = Stmt.new(c)
-#s4.indexes(ARGV[2], ARGV[3])
-#pp s4.error(1)
