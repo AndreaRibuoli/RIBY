82e61220499e5cd7e6d867b948e0de823d7294b0
Andrea Ribuoli <andrea@powerruby.com>
2021-04-28 12:40:23 +0200

    bind

--- riby_testD.rb
+++ riby_testD.rb
@@ -11,7 +11,15 @@ pp c.empower(ARGV[0], ARGV[1])
 s = Stmt.new(c)
 #s.tables(ARGV[2], ARGV[3], ARGV[4])
 #s.pkeys(ARGV[2], ARGV[3])
-#s.fkeys_using(ARGV[2], ARGV[3])
+s.fkeys_using(ARGV[2], ARGV[3])
+n = s.columns_count[:SQL_DESC_COUNT]
+puts n
+cols = []
+n.times {|i| seq = i+1; cols << Column.new(s, seq, s.column_data(seq)) }
+cols.each { |f| f.bind }
+while s.fetch == 0
+  cols.each { |f| pp f.buffer }
+end
 s.fkeys_used_by(ARGV[2], ARGV[3])
 n = s.columns_count[:SQL_DESC_COUNT]
 puts n
@@ -21,4 +29,3 @@ cols.each { |f| f.bind }
 while s.fetch == 0
   cols.each { |f| pp f.buffer }
 end
-pp s.error
