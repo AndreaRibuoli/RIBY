e455679a12a702c28af708d235bb4399974d4a4a
Andrea Ribuoli <andrea@powerruby.com>
2021-04-28 09:54:12 +0200

    bind

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -1081,7 +1081,11 @@ class Column
     @hstmt = hstmt
     @icol = seq
     @desc = desc
-    @desc[:SQL_BIND_TYPE] = SQL_WCHAR
+    if @desc[:SQL_DESC_TYPE_NAME] == 'VARCHAR' && @desc[:SQL_DESC_COLUMN_CCSID] == 65535
+      @desc[:SQL_BIND_TYPE] = SQL_VARBINARY
+    else
+      @desc[:SQL_BIND_TYPE] = SQL_WCHAR
+    end
     hstmt.add(seq)
     ObjectSpace.define_finalizer(self, Stmt.finalizer_proc(seq,hstmt))
   end
@@ -1117,11 +1121,7 @@ class Column
     ileArguments[  36,  2] = [@icol].pack("s*")
     ileArguments[  38,  2] = @desc[:SQL_BIND_TYPE]
     ileArguments[  40,  8] = PAD_08
-    if @desc[:SQL_DESC_TYPE_NAME] == 'VARCHAR' && @desc[:SQL_DESC_COLUMN_CCSID] == 65535
-      ileArguments[  48, 16] = [ 0, 0].pack("q*")
-    else
-      ileArguments[  48, 16] = [ 0, @buffer.to_i].pack("q*")
-    end
+    ileArguments[  48, 16] = [ 0, @buffer.to_i].pack("q*")
     ileArguments[  64,  4] = [@buffer.instance_variable_get(:@entity).size].pack("l*")
     ileArguments[  68, 12] = PAD_12
     ileArguments[  80, 16] = [0, @pcbValue.to_i].pack("q*")
