81d59b7d1822484f92aaf08b6763befea36cfdcc
Andrea Ribuoli <andrea@powerruby.com>
2021-04-29 04:51:43 +0200

    bind

--- riby_testD.rb
+++ riby_testD.rb
@@ -9,8 +9,28 @@ e.attrs = { :SQL_ATTR_SERVER_MODE => :SQL_TRUE }
 c = Connect.new(e)
 pp c.empower(ARGV[0], ARGV[1])
 s = Stmt.new(c)
-#s.tables(ARGV[2], ARGV[3], ARGV[4])
-#s.pkeys(ARGV[2], ARGV[3])
+s.tables(ARGV[2], ARGV[3], ARGV[4])
+n = s.columns_count[:SQL_DESC_COUNT]
+puts n
+cols = []
+n.times {|i| seq = i+1; cols << Column.new(s, seq, s.column_data(seq)) }
+cols.each { |f| f.bind }
+while s.fetch == 0
+  row = ''
+  cols.each { |f| row << f.buffer << ', ' }
+  pp row
+end
+s.pkeys(ARGV[2], ARGV[3])
+n = s.columns_count[:SQL_DESC_COUNT]
+puts n
+cols = []
+n.times {|i| seq = i+1; cols << Column.new(s, seq, s.column_data(seq)) }
+cols.each { |f| f.bind }
+while s.fetch == 0
+  row = ''
+  cols.each { |f| row << f.buffer << ', ' }
+  pp row
+end
 s.fkeys_using(ARGV[2], ARGV[3])
 n = s.columns_count[:SQL_DESC_COUNT]
 puts n
