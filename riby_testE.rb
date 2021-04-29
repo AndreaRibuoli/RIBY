2162049c2b7efb5ba2e1186b8e48d00476f2680a
Andrea Ribuoli <andrea@powerruby.com>
2021-04-29 20:31:50 +0200

    33

---
+++ riby_testE.rb
@@ -0,0 +1,23 @@
+#! /QOpenSys/pkgs/bin/ruby
+require_relative 'riby_qsqcli'
+require 'pp'
+                                                                       
+raise "Usage: #{__FILE__} <user> <password> <sch> <nam> <typ>" if ARGV.length != 5
+                                                                       
+e = Env.new
+e.attrs = { :SQL_ATTR_SERVER_MODE => :SQL_TRUE }
+c = Connect.new(e)
+c.empower(ARGV[0], ARGV[1])
+s = Stmt.new(c)
+puts "==== s.languages() =========================================================================="
+pp s.languages()
+pp s.error
+n = s.columns_count[:SQL_DESC_COUNT]
+cols = []
+n.times {|i| seq = i+1; cols << Column.new(s, seq, s.column_data(seq)) }
+cols.each { |f| f.bind; pp s.error }
+while s.fetch == 0
+  row = ''
+  cols.each { |f| row << f.buffer.to_s << ', ' }
+  pp row
+end
