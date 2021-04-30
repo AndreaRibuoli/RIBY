73ddcdbea43eaa6029de6aa245ecbd8bef5be3d3
Andrea Ribuoli <andrea@powerruby.com>
2021-04-30 09:14:06 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -1184,14 +1184,14 @@ class Column
     ileArguments[   0, 32] = PAD_32
     ileArguments[  32,  4] = @hstmt.handle
     ileArguments[  36,  2] = [@icol].pack("s*")
-    ileArguments[  38,  2] = @desc[:SQL_BIND_TYPE]
+    ileArguments[  38,  2] = SQL_BINARY
     ileArguments[  40,  8] = PAD_08
     ileArguments[  48, 16] = [ 0, tmpbuffer.to_i].pack("q*")
     ileArguments[  64,  4] = [tmpbuffer.instance_variable_get(:@entity).size].pack("l*")
     ileArguments[  68, 12] = PAD_12
     ileArguments[  80, 16] = [0, pcbValue.to_i].pack("q*")
-#   Ilecallx.call(SQLApis['SQLGetColW'], ileArguments, SQLApiList['SQLGetColW'], - 5, 0)
-    Ilecallx.call(SQLApis['SQLGetCol'], ileArguments, SQLApiList['SQLGetCol'], - 5, 0)
+    Ilecallx.call(SQLApis['SQLGetColW'], ileArguments, SQLApiList['SQLGetColW'], - 5, 0)
+ #  Ilecallx.call(SQLApis['SQLGetCol'], ileArguments, SQLApiList['SQLGetCol'], - 5, 0)
     rc = ileArguments[ 16, 4].unpack('l')[0]
     case
       when pcbValue[0, 4] == SQL_NTS
