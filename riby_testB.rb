89f183e9c8d5b36584c8538853dae83ca2cf571c
Andrea Ribuoli <andrea@powerruby.com>
2021-04-30 17:44:09 +0200

    33

--- riby_testB.rb
+++ riby_testB.rb
@@ -10,49 +10,36 @@ c = Connect.new(e)
 c.empower(ARGV[0], ARGV[1])
 s = Stmt.new(c)
 s.attrs = { :SQL_ATTR_EXTENDED_COL_INFO => :SQL_TRUE }
-# s.execdirect(ARGV[2])
-# pp s.error
 s.prepare(ARGV[2])
-#pp s.error
 n = s.columns_count[:SQL_DESC_COUNT]
 cols = []
 n.times {|i|
   seq = i+1
    cols << Column.new(s, seq, s.column_data(seq))
 }
-#cols.each { |f|
-#  f.bind  if f.seq > 8
-#}
 s.execute
-#pp s.error
-
+puts "Without bind using get"
 while s.fetch == 0
   row = ''
-  cols.each { |f| row << f.get.to_s << ', '; pp s.error }
+  cols.each { |f| row << f.get.to_s << ', '}
   pp row
 end
 cols = []
 s.close
-pp s.error
 s.prepare(ARGV[2])
-pp s.error
 n = s.columns_count[:SQL_DESC_COUNT]
-pp s.error
 n.times {|i|
   seq = i+1
-   cols << Column.new(s, seq, s.column_data(seq))
-   pp s.error
+  cols << Column.new(s, seq, s.column_data(seq))
 }
 cols.each { |f|
-  f.bind  
-  pp s.error
+  f.bind
 }
 s.execute
-pp s.error
-
+puts "With bind using buffer"
 while s.fetch == 0
   row = ''
-  cols.each { |f| row << f.buffer.to_s << ', '; pp s.error }
+  cols.each { |f| row << f.buffer.to_s << ', '}
   pp row
 end
 
