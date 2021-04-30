d5673cc94fa48100acbaed2d482a69bfe7c655d9
Andrea Ribuoli <andrea@powerruby.com>
2021-04-30 22:07:38 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -1203,7 +1203,7 @@ SQL_VARGRAPHIC               = [96].pack("s*")
     @icol
   end
   def bind
- #   @desc[:SQL_BIND_TYPE] = SQL_WCHAR    if @desc[:SQL_BIND_TYPE] = SQL_CHAR
+    @desc[:SQL_BIND_TYPE] = SQL_WCHAR    if @desc[:SQL_BIND_TYPE] = SQL_CHAR
     @desc[:SQL_BIND_TYPE] = SQL_WVARCHAR if @desc[:SQL_BIND_TYPE] = SQL_VARCHAR
     SQLBindCol()
   end
