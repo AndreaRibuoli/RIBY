df50a1f22da75fc60eebeaf2d5a40990027ab192
Andrea Ribuoli <andrea@powerruby.com>
2021-04-30 15:27:37 +0200

    33

--- riby_testB.rb
+++ riby_testB.rb
@@ -7,13 +7,13 @@ raise "Usage: #{__FILE__} <user> <password> <sql>" if ARGV.length != 3
 e = Env.new
 e.attrs = { :SQL_ATTR_SERVER_MODE => :SQL_TRUE }
 c = Connect.new(e)
-pp c.empower(ARGV[0], ARGV[1])
+c.empower(ARGV[0], ARGV[1])
 s = Stmt.new(c)
 s.attrs = { :SQL_ATTR_EXTENDED_COL_INFO => :SQL_TRUE }
 # s.execdirect(ARGV[2])
 # pp s.error
 s.prepare(ARGV[2])
-pp s.error
+#pp s.error
 n = s.columns_count[:SQL_DESC_COUNT]
 cols = []
 n.times {|i|
@@ -24,10 +24,7 @@ n.times {|i|
 #  f.bind  if f.seq > 8
 #}
 s.execute
-pp s.error
-
-pp s.fetch
-# pp s.error
+#pp s.error
 
 while s.fetch == 0
   row = ''
