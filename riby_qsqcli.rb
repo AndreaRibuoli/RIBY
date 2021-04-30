a9b403096cca7e23cf116d6a44ba4a18d105e9ab
Andrea Ribuoli <andrea@powerruby.com>
2021-04-30 10:41:58 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -1131,6 +1131,8 @@ class Column
         @desc[:SQL_BIND_TYPE] = SQL_VARBINARY
       when @desc[:SQL_DESC_TYPE_NAME] == 'CHAR'
         @desc[:SQL_BIND_TYPE] = SQL_CHAR
+      when @desc[:SQL_DESC_TYPE_NAME] == 'DATE'
+          @desc[:SQL_BIND_TYPE] = SQL_DATE
       when @desc[:SQL_DESC_TYPE_NAME] == 'VARCHAR'
           @desc[:SQL_BIND_TYPE] = SQL_VARCHAR
       when @desc[:SQL_DESC_TYPE_NAME] == 'INTEGER'
@@ -1209,7 +1211,7 @@ class Column
     case
       when @desc[:SQL_BIND_TYPE] == SQL_INTEGER
         return tmpbuffer[0, 4].unpack("l*")[0]
-      when @desc[:SQL_BIND_TYPE] == SQL_CHAR
+      when @desc[:SQL_BIND_TYPE] == SQL_CHAR || @desc[:SQL_BIND_TYPE] == SQL_DATE
         enc = 'IBM037' if @desc[:SQL_DESC_COLUMN_CCSID] == 37
         enc = 'IBM280' if @desc[:SQL_DESC_COLUMN_CCSID] == 280
         enc = 'IBM1144' if @desc[:SQL_DESC_COLUMN_CCSID] == 1144
