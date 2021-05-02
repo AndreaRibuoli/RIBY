d306deee21ba44339321a65830414096914623c1
Andrea Ribuoli <andrea@powerruby.com>
2021-05-03 01:08:52 +0200

    33

--- riby_testB.rb
+++ riby_testB.rb
@@ -41,10 +41,10 @@ if ARGV[3] == 'BIND'
   n.times {|i| seq = i+1; cols << Column.new(s, seq, s.column_data(seq)) }
   cols.each { |f| f.bind }
   m = s.numparams
-  d1 = Desc.new(s)
-  pp d1.desc_data(1)
-  d2 = Desc.new(s, false)
-  pp d2.desc_data(2)
+#  d1 = Desc.new(s)
+#  pp d1.desc_data(1)
+#  d2 = Desc.new(s, false)
+#  pp d2.desc_data(2)
   pars = []
   m.times {|i| seq = i+1; pars << Param.new(s, seq, s.param_data(seq)) }
   pars.each { |f| f.bind }
