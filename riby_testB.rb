afb3ef032b5e433dc9c898f6c5bd6864b9d1ea47
Andrea Ribuoli <andrea@powerruby.com>
2021-05-04 21:48:33 +0200

    33

--- riby_testB.rb
+++ riby_testB.rb
@@ -17,7 +17,9 @@ if ARGV[3] == 'GET'
   cols = []
   dc = Desc.new(s, false)
   dci = Desc.new(s, false, false)
-  n.times {|i| seq = i+1; puts dci.desc_data(seq)[:SQL_DESC_NAME] }
+  head = ''
+  n.times {|i| seq = i+1; head << dci.desc_data(seq)[:SQL_DESC_NAME] << ', ' }
+  puts head
   n.times {|i| seq = i+1; cols << Column.new(s, seq, dc.desc_data(seq), dci.desc_data(seq)) }
   m = s.numparams
   pars = []
@@ -42,7 +44,9 @@ if ARGV[3] == 'BIND'
   cols = []
   dc  = Desc.new(s, false)
   dci = Desc.new(s, false, false)
-  n.times {|i| seq = i+1; puts dci.desc_data(seq)[:SQL_DESC_NAME] }
+  head = ''
+  n.times {|i| seq = i+1; head << dci.desc_data(seq)[:SQL_DESC_NAME] << ', ' }
+  puts head
   n.times {|i| seq = i+1; cols << Column.new(s, seq, dc.desc_data(seq), dci.desc_data(seq)) }
   cols.each { |f| f.bind }
   m = s.numparams
