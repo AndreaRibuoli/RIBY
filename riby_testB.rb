43dfacd695cd929262aafea227aae1a007b694a7
Andrea Ribuoli <andrea@powerruby.com>
2021-05-04 20:58:46 +0200

    33

--- riby_testB.rb
+++ riby_testB.rb
@@ -55,14 +55,11 @@ if ARGV[3] == 'BIND'
   pars = []
   dp  = Desc.new(s)
   dp.set(1, :SQL_DESC_TYPE, :SQL_WCHAR)
-# dp.set(1, :SQL_DESC_LENGTH, 16)
-# dp.set(1, :SQL_DESC_CCSID, 1200)
   dpi = Desc.new(s, true, false)
-  dpi.set(1, :SQL_DESC_TYPE, :SQL_WCHAR)
   m.times {|i| seq = i+1; pars << Param.new(s, seq, dp.desc_data(seq), dpi.desc_data(seq)) }
   pars.each { |f| f.bind }
-  pars[0].buffer= ARGV[4].encode('UTF-16BE')
-  pars[0].pcbValue= 16
+  pars[0].buffer= ARGV[4].encode('IBM280')
+  pars[0].pcbValue= 8
   puts "With bind using buffer"
   s.execute
   while s.fetch == 0
