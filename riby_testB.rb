5e976771c74e02148478874cc27d1b25ed81a845
Andrea Ribuoli <andrea@powerruby.com>
2021-05-04 20:47:35 +0200

    33

--- riby_testB.rb
+++ riby_testB.rb
@@ -55,9 +55,8 @@ if ARGV[3] == 'BIND'
   pars = []
   dp  = Desc.new(s)
   dp.set(1, :SQL_DESC_CCSID, 1208)
+  dp.set(1, :SQL_DESC_TYPE, :SQL_CHAR)
   dpi = Desc.new(s, true, false)
-  dpi.set(1, :SQL_DESC_CCSID, 1208)
-  pp s.error
   m.times {|i| seq = i+1; pars << Param.new(s, seq, dp.desc_data(seq), dpi.desc_data(seq)) }
   pars.each { |f| f.bind }
   pars[0].buffer= ARGV[4].encode('IBM280')
