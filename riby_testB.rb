1b14b482794a8c5c4d5a66f86bdb5fd1d6b021d2
Andrea Ribuoli <andrea@powerruby.com>
2021-05-04 20:55:06 +0200

    33

--- riby_testB.rb
+++ riby_testB.rb
@@ -55,6 +55,7 @@ if ARGV[3] == 'BIND'
   pars = []
   dp  = Desc.new(s)
   dp.set(1, :SQL_DESC_TYPE, :SQL_WCHAR)
+  dp.set(1, :SQL_DESC_LENGTH, 16)
 # dp.set(1, :SQL_DESC_CCSID, 1200)
   dpi = Desc.new(s, true, false)
   m.times {|i| seq = i+1; pars << Param.new(s, seq, dp.desc_data(seq), dpi.desc_data(seq)) }
