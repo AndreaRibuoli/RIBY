06224e6ca8b94c0d0200a332626dc3793b5d53a6
Andrea Ribuoli <andrea@powerruby.com>
2021-05-04 15:49:31 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -1426,7 +1426,6 @@ class Column
            ZEROED[0, tmpbuffer.instance_variable_get(:@entity).size]
         return tbr
       when pcbValue[0, 4] == SQL_NULL_HANDLE
-        puts @desc[:SQL_DESC_TYPE].unpack('H*')
         return tmpbuffer[2, @desc[:SQL_DESC_LENGTH]-2].force_encoding('IBM037').encode('utf-8').strip
       else
         return "error: pcbValue #{pcbValue[0, 4].unpack("l*")[0]}"
