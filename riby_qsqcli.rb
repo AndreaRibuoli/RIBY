42f7acee070aa77c476c41af9a4c6998792abe95
Andrea Ribuoli <andrea@powerruby.com>
2021-05-05 12:38:33 +0200

    33

--- riby_qsqcli.rb
+++ riby_qsqcli.rb
@@ -1153,7 +1153,7 @@ class Desc
     return { fldi => "return code = #{rc}"} if rc != 0
     case
       when (t = SQLDescVals[:VALDESC_DECO][fldi]) != nil
-        puts "\nIl valore #{buffer[0, 2].unpack('s')[0]} restituisce #{t.key(buffer[0, 2].unpack('s')[0]}"
+        puts "\nIl valore #{buffer[0, 2].unpack('s')[0]} restituisce #{t.key(buffer[0, 2].unpack('s')[0])}\n"
         return { fldi => t.key(buffer[0, 2].unpack("s")[0]) }
       when (t = SQLDescVals[:VALDESC_DECO_INT][fldi]) != nil
         return { fldi => t.key(buffer[0, 4].unpack("l")[0]) }
