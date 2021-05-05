048ae2399f81cdd98d4d7b11e4b6b059d5393d89
Andrea Ribuoli <andrea@powerruby.com>
2021-05-05 22:41:46 +0200

    34

--- riby_testB.rb
+++ riby_testB.rb
@@ -36,6 +36,7 @@ pars.each { |f| f.bind }
 pars[0].buffer= ARGV[3].encode('UTF-16BE')               # .encode('IBM280')
 pars[0].pcbValue= ARGV[3].length * 2
 s.execute
+pp s.error
 records = [head]
 while s.fetch == 0
   row = []
